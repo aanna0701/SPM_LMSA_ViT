885141f544edcee74b85cfe8013e8a1b8c1706c9
root <aanna0701@gmail.com>
2021-03-19 02:57:25 +0000

    fix: train.sh

--- train.sh
+++ train.sh
@@ -1,12 +1,9 @@
 #!/bin/zsh
 
-ARRAY_MODEL=("resnet56", "nlb2")
-ARRAY_SEED=(1, 2, 3)
-
-for m in "resnet56" "nlb2"
+for m in "resnet56" "nlb1" "nlb2" "nlb3" "nlb4"
 do
 	for s in 1 2 3
 	do
-		python train.py --lr 0.1 --model ${m} --seed ${s}
+		python train.py --dataset_dir /workspace/dataset --lr 0.1 --model ${m} --seed ${s}
 	done
 done
