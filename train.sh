833ce5d255348c8377cddf48a943b2bf2590c8b5
Lee Seung Hoon <aanna0701@gmail.com>
2021-03-26 16:55:24 +0900

    update: tarin.sh

--- train.sh
+++ train.sh
@@ -2,9 +2,9 @@
 
 for s in 1 2 3 4 5
 do
-	for m in 12 15 18 21 24 27
+	for m in 1 2 3 4 5 6 7 8 9
 	do
-		python train.py --dataset_dir /workspace/dataset --lr 0.05 --model sa --seed ${s} --n_blocks ${m}
-		python train.py --dataset_dir /workspace/dataset --lr 0.05 --model swga --seed ${s} --n_blocks ${m}
+		python train.py --dataset_dir /workspace/dataset --lr 0.1 --model sa --seed ${s} --n_blocks ${m}
+		python train.py --dataset_dir /workspace/dataset --lr 0.1 --model swga --seed ${s} --n_blocks ${m}
 	done
 done
