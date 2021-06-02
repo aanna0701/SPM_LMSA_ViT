826c44f104e8a2065bb33c8ae49c26e3accfbf25
Lee Seung Hoon <aanna0701@gmali.com>
2021-06-03 01:20:55 +0900

    update

--- deit/datasets.py
+++ deit/datasets.py
@@ -124,9 +124,9 @@ def build_transform(is_train, args):
 
     t.append(transforms.ToTensor())
     if args.data_set == 'CIFAR10':
-        t.append(transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)), RandAugment(1, 5))
+        t.append([transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)), RandAugment(1, 5)])
     elif args.data_set == 'CIFAR100':
-        t.append(transforms.Normalize((0.5071, 0.4865, 0.4409), (0.2673, 0.2564, 0.2762)), RandAugment(1, 5))
+        t.append([transforms.Normalize((0.5071, 0.4865, 0.4409), (0.2673, 0.2564, 0.2762)), RandAugment(1, 5)])
     elif args.data_set == 'IMNET':
         t.append(transforms.Normalize(IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD))
     return transforms.Compose(t)
