4f23cd508e7984010104373cc493614d01496f5f
Lee Seung Hoon <aanna0701@gmali.com>
2021-06-04 22:02:41 +0900

    update

--- Compact-Transformers/train.sh
+++ Compact-Transformers/train.sh
@@ -1,10 +1,18 @@
 #!/bin/zsh
 
+# for s in 1 2 3
+# do
+# 	# python main.py --depth 12 --channel 192 --heads 3 --gpu 1 --lr 0.0005 --model DeiT --tag deit_test --seed ${s}
+# 	python main.py --depth 9 --channel 192 --heads 3 --gpu 0 --lr 0.0005 --model DeiT --tag deit_test --seed ${s}
+# 	python main.py --depth 6 --channel 192 --heads 3 --gpu 0 --lr 0.0005 --model DeiT --tag deit_test --seed ${s}
+# 	# python main.py --depth 3 --channel 192 --heads 3 --gpu 1 --lr 0.0005 --model DeiT --tag deit_test --seed ${s}
+# done
+
 for s in 1 2 3
 do
-	# python main.py --depth 12 --channel 192 --heads 3 --gpu 1 --lr 0.0005 --model DeiT --tag deit_test --seed ${s}
-	python main.py --depth 9 --channel 192 --heads 3 --gpu 0 --lr 0.0005 --model DeiT --tag deit_test --seed ${s}
-	python main.py --depth 6 --channel 192 --heads 3 --gpu 0 --lr 0.0005 --model DeiT --tag deit_test --seed ${s}
-	# python main.py --depth 3 --channel 192 --heads 3 --gpu 1 --lr 0.0005 --model DeiT --tag deit_test --seed ${s}
+	python main.py --depth 6 --channel 48 --heads 3 --gpu 1 --lr 0.003 --model ViT --tag vit_base --seed ${s}
+	# python main.py --depth 6 --channel 64 --heads 4 --gpu 0 --lr 0.003 --model ViT --tag vit_base --seed ${s}
+	# python main.py --depth 6 --channel 80 --heads 5 --gpu 0 --lr 0.003 --model ViT --tag vit_base --seed ${s}
+	python main.py --depth 6 --channel 96 --heads 6 --gpu 1 --lr 0.003 --model ViT --tag vit_base --seed ${s}
 done
 
