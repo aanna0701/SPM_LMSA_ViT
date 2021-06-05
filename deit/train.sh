7c352c5e998fa56771febff8a2264d0fed645e4b
Lee Seung Hoon <aanna0701@gmali.com>
2021-06-06 01:48:34 +0900

    update

--- deit/train.sh
+++ deit/train.sh
@@ -10,10 +10,10 @@
 
 for s in 2 3
 do
-#	python main.py --depth 6 --channel 48 --heads 3 --gpu 1 --lr 0.003 --model g-vit --tag g-vit_base_scale --seed ${s}
-	python main.py --depth 6 --channel 64 --heads 4 --gpu 0 --lr 0.003 --model g-vit --tag g-vit_base_scale --seed ${s}
-	python main.py --depth 6 --channel 80 --heads 5 --gpu 0 --lr 0.003 --model g-vit --tag g-vit_base_scale --seed ${s}
-#	python main.py --depth 6 --channel 96 --heads 6 --gpu 1 --lr 0.003 --model g-vit --tag g-vit_base_scale --seed ${s}
+#	python main.py --depth 6 --channel 48 --heads 3 --gpu 1 --lr 0.003 --model deit --tag test --seed ${s}--data_set CIFAR10
+	python main.py --depth 6 --channel 64 --heads 4 --gpu 0 --lr 0.003 --model deit --tag test --seed ${s} --data_set CIFAR10
+	python main.py --depth 6 --channel 80 --heads 5 --gpu 0 --lr 0.003 --model deit --tag test --seed ${s} --data_set CIFAR10
+#	python main.py --depth 6 --channel 96 --heads 6 --gpu 1 --lr 0.003 --model deit --tag test --seed ${s} --data_set CIFAR10
 done
-python main.py --depth 6 --channel 80 --heads 5 --gpu 0 --lr 0.003 --model g-vit --tag g-vit_base_scale --seed 1
+# python main.py --depth 6 --channel 80 --heads 5 --gpu 0 --lr 0.003 --model g-vit --tag g-vit_base_scale --seed 1
 #python main.py --depth 6 --channel 96 --heads 6 --gpu 1 --lr 0.003 --model g-vit --tag g-vit_base_scale --seed 1
