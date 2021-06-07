9a929f6a2bb61383ea90f239d048c1b1e754cd30
Lee Seung Hoon <aanna0701@gmali.com>
2021-06-08 00:57:08 +0900

    update

--- models/vits_pooling.py
+++ models/vits_pooling.py
@@ -624,7 +624,6 @@ class ViT_pooling(nn.Module):
         
         num_nodes = (in_size - patch_size + 2) // (patch_size // 2) + 1  
         self.in_size = (num_nodes * num_nodes + 1, inter_dimension)
-        print((in_size - patch_size + 2))
 
         self.patch_embedding = Patch_Embedding(
             patch_size=patch_size, in_channels=3, inter_channels=inter_dimension)
