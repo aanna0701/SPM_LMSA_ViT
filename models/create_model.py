2c7253a2dce9293e8f03716434879b94974a2a7e
Lee Seung Hoon <aanna0701@gmali.com>
2021-06-07 23:25:34 +0900

    update

--- models/create_model.py
+++ models/create_model.py
@@ -64,9 +64,9 @@ def P_GiT_conv(num_blocks, GA=True, num_classes=10, dropout=True, pooling='conv'
 
 def P_GiT_conv_imgnet(num_blocks, GA=True, num_classes=10, dropout=True, pooling='conv', in_size=224, patch_size=16):
     if num_blocks == 1:
-        return ViT_pooling(in_size=in_size, patch_size=patch_size, inter_dimension=64, num_blocks=[2, 6, 4], heads=2, mlp_ratio=4, GA=GA, dropout=dropout, pooling=pooling, num_classes=num_classes)
+        return ViT_pooling(in_size=in_size, patch_size=patch_size, inter_dimension=64, num_blocks=[2, 6, 4], heads=2, mlp_ratio=2, GA=GA, dropout=dropout, pooling=pooling, num_classes=num_classes)
     elif num_blocks == 2:
-        return ViT_pooling(in_size=in_size, patch_size=patch_size, inter_dimension=24, num_blocks=[1, 3, 2], heads=2, mlp_ratio=4, GA=GA, dropout=dropout, pooling=pooling, num_classes=num_classes)
+        return ViT_pooling(in_size=in_size, patch_size=patch_size, inter_dimension=24, num_blocks=[1, 3, 2], heads=2, mlp_ratio=2, GA=GA, dropout=dropout, pooling=pooling, num_classes=num_classes)
     elif num_blocks == 3:
-        return ViT_pooling(in_size=in_size, patch_size=patch_size, inter_dimension=30, num_blocks=[1, 3, 2], heads=3, mlp_ratio=4, GA=GA, dropout=dropout, pooling=pooling, num_classes=num_classes)
+        return ViT_pooling(in_size=in_size, patch_size=patch_size, inter_dimension=30, num_blocks=[1, 3, 2], heads=3, mlp_ratio=2, GA=GA, dropout=dropout, pooling=pooling, num_classes=num_classes)
    
