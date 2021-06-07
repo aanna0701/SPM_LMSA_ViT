220c1ab6b926fad7ef5e502fb947f064f7e530f8
Lee Seung Hoon <aanna0701@gmali.com>
2021-06-07 11:31:37 +0900

    update

--- train.sh
+++ train.sh
@@ -1,8 +1,19 @@
+#!/bin/zsh
 
-for s in 3 4 5
+#for s in 1 2 3
+#do
+#	python main.py --depth 1 --gpu 1 --lr 0.003 --model g-pit --tag g-vit_LS_AA_pooling --seed ${s} --label_smoothing
+	# python main.py --depth 2 --gpu 0 --lr 0.003 --model g-pit --tag g-vit_LS_AA_pooling --seed ${s} --label_smoothing
+	# python main.py --depth 3 --gpu 0 --lr 0.003 --model g-pit --tag g-vit_LS_AA_pooling --seed ${s} --label_smoothing
+#	python main.py --depth 4 --gpu 1 --lr 0.003 --model g-pit --tag g-vit_LS_AA_pooling --seed ${s} --label_smoothing
+#done
+
+for s in 2 3
 do
-	# python train.py --dataset_dir /workspace/dataset --gpu 1 --lr 0.003 --model vit --seed ${s} --depth 6 --channel 48 --heads 3 --tag ViT_base --dataset CIFAR10
-	python train.py --dataset_dir /workspace/dataset --gpu 0 --lr 0.003 --model vit --seed ${s} --depth 6 --channel 64 --heads 4 --tag ViT_base --dataset CIFAR10
-	python train.py --dataset_dir /workspace/dataset --gpu 0 --lr 0.003 --model vit --seed ${s} --depth 6 --channel 80 --heads 5 --tag ViT_base --dataset CIFAR10
-	# python train.py --dataset_dir /workspace/dataset --gpu 1 --lr 0.003 --model vit --seed ${s} --depth 6 --channel 96 --heads 6 --tag ViT_base --dataset CIFAR10
+#	python main.py --depth 6 --channel 48 --heads 3 --gpu 1 --lr 0.003 --model deit --tag AA_LS_CJ --seed ${s} --enable_deit --label_smoothing
+	python main.py --depth 6 --channel 64 --heads 4 --gpu 0 --lr 0.003 --model deit --tag AA_LS_CJ --seed ${s} --enable_deit --label_smoothing
+	python main.py --depth 6 --channel 80 --heads 5 --gpu 0 --lr 0.003 --model deit --tag AA_LS_CJ --seed ${s} --enable_deit --label_smoothing
+# 	python main.py --depth 6 --channel 96 --heads 6 --gpu 1 --lr 0.003 --model deit --tag AA_LS_CJ --seed ${s} --enable_deit --label_smoothing
 done
+ #python main.py --depth 6 --channel 80 --heads 5 --gpu 0 --lr 0.003 --model g-vit --tag g-vit_base_scale --seed 1
+# python main.py --depth 6 --channel 96 --heads 6 --gpu 1 --lr 0.003 --model g-vit --tag g-vit_base_scale --seed 1
