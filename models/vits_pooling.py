88f7f80dddaaa55939af5f0207bb063309d579bb
Lee Seung Hoon <aanna0701@gmali.com>
2021-06-07 20:51:17 +0900

    update

--- models/vits_pooling.py
+++ models/vits_pooling.py
@@ -6,6 +6,187 @@ import math
 from models.vits import GA_block
 
 
+class MHSA(nn.Module):
+    def __init__(self, in_channels, heads=4):
+        super(MHSA, self).__init__()
+        self.heads = heads
+
+        self.query = nn.Linear(in_channels, in_channels, bias=False)
+        self._init_weights(self.query)
+        self.key = nn.Linear(in_channels, in_channels, bias=False)
+        self._init_weights(self.key)
+        self.value = nn.Linear(in_channels, in_channels, bias=False)
+        self._init_weights(self.value)
+        self.out = nn.Linear(in_channels, in_channels, bias=False)
+        self._init_weights(self.out)
+        self.dropout = nn.Dropout(0.1)
+        self.softmax = nn.Softmax(dim=-1)
+        self.edge = None
+        
+    def _init_weights(self,layer):
+        nn.init.kaiming_normal_(layer.weight)
+        if layer.bias:
+            nn.init.normal_(layer.bias, std=1e-6)
+
+    def forward(self, x, dropout=True):
+        '''
+        [shapes]
+
+            * C = heads * inter_dimension
+
+            x : (B, HW+1, C)            
+            q : (B, heads, HW+1, inter_dimension)
+            k : (B, heads, HW+1, inter_dimension)
+            v : (B, heads, HW+1, inter_dimension)
+            similarity : (B, heads, HW+1, HW+1)
+            out_tmp : (B, heads, HW+1, inter_dimension)
+            out : (B, HW+1, C)
+
+        '''
+
+
+        B, n_tokens, C = x.size()
+
+        inter_dimension = C // self.heads
+
+        q = self.query(x).view(B, n_tokens, self.heads,
+                               inter_dimension).permute(0, 2, 1, 3)
+        k = self.key(x).view(B, n_tokens, self.heads,
+                             inter_dimension).permute(0, 2, 1, 3)
+        v = self.value(x).view(B, n_tokens, self.heads,
+                               inter_dimension).permute(0, 2, 1, 3)
+
+        similarity = torch.matmul(q, k.transpose(3, 2))
+        self.edge = similarity
+        similarity = similarity / math.sqrt(inter_dimension)
+
+        attention = self.softmax(similarity)
+
+        out_tmp = torch.matmul(attention, v)
+        out = out_tmp.permute(0, 2, 1, 3).contiguous()
+        out = out.view(B, n_tokens, -1)
+        out = self.out(out)
+        if dropout:
+            out = self.dropout(out)
+            
+        return out
+
+
+class Positional_Embedding(nn.Module):
+    def __init__(self, spatial_dimension, inter_channels):
+        super(Positional_Embedding, self).__init__()
+        '''
+        [shape]
+            self.PE : (1, HW+1, C)
+        '''
+        self.PE = nn.Parameter(torch.zeros(
+            1, spatial_dimension, inter_channels))
+        
+
+    def forward(self, x):
+        return x.permute(0, 2, 1) + self.PE
+
+
+class Patch_Embedding(nn.Module):
+    def __init__(self, patch_size, in_channels, inter_channels):
+        super(Patch_Embedding, self).__init__()
+        self.patch_embedding = nn.Conv2d(in_channels, inter_channels,
+                                         kernel_size=patch_size, stride= patch_size // 2, padding=0, bias=False)
+        self._init_weights(self.patch_embedding)
+        self.inter_channels = inter_channels
+
+    def _init_weights(self,layer):
+        nn.init.kaiming_normal_(layer.weight)
+        if layer.bias:
+            nn.init.normal_(layer.bias, std=1e-6)
+
+    def forward(self, x):
+        '''
+        [shapes]
+            x : (B, C, H, W)
+            out : (B, C', H, W)
+            out_flat : (B, C', HW)
+        '''
+        out = self.patch_embedding(x)
+        out_flat = out.flatten(start_dim=2)
+        
+        return out_flat
+
+
+class MLP(nn.Module):
+
+    def __init__(self, in_channels, hidden_dim_ratio):
+        super(MLP, self).__init__()
+        self.in_channels = in_channels
+        self.inter_channels = self.in_channels * hidden_dim_ratio
+
+        self.fc1 = nn.Linear(self.in_channels, self.inter_channels, bias=False)
+        self._init_weights(self.fc1)
+        self.fc2 = nn.Linear(self.inter_channels, self.in_channels, bias=False)
+        self._init_weights(self.fc2)
+        self.dropout = nn.Dropout(0.1)
+        
+
+    def _init_weights(self,layer):
+        nn.init.kaiming_normal_(layer.weight)
+        if layer.bias:
+            nn.init.normal_(layer.bias, std=1e-6)
+
+    def forward(self, x, dropout=True):
+        '''
+        [shape]
+            x : (B, HW+1, C)
+            x_inter : (B, HW+1, 4*C)
+            x_out : (B, HW+1, C)
+        '''
+
+        x_inter = F.gelu(self.fc1(x))
+        if dropout:
+            x_inter = self.dropout(x_inter)
+
+        x_out = self.fc2(x_inter)
+        if dropout:
+            x_out = self.dropout(x_out)
+
+        return x_out
+
+
+class MLP_GA(nn.Module):
+
+    def __init__(self, in_channels, out_channels, compression_ratio=4):
+        super(MLP_GA, self).__init__()
+        self.in_channels = in_channels
+        self.inter_channels = self.in_channels // compression_ratio
+        self.out_channels = out_channels
+
+        self.fc1 = nn.Linear(self.in_channels, self.inter_channels, bias=False)
+        self._init_weights(self.fc1)
+        self.fc2 = nn.Linear(self.inter_channels, self.out_channels, bias=False)
+        self._init_weights(self.fc2)
+        self.dropout = nn.Dropout(0.1)
+
+    def _init_weights(self,layer):
+        nn.init.kaiming_normal_(layer.weight)
+        if layer.bias:
+            nn.init.normal_(layer.bias, std=1e-6)
+
+    def forward(self, x):
+        '''
+        [shape]
+            x : (B, HW+1, C)
+            x_inter : (B, HW+1, 4*C)
+            x_out : (B, HW+1, C)
+        '''
+
+        x_inter = F.gelu(self.fc1(x))
+        # if dropout:
+        #     x_inter = self.dropout(x_inter)
+
+        x_out = self.fc2(x_inter)
+        # if dropout:
+        #     x_out = self.dropout(x_out)
+
+        return x_out
 
 
 # class Pooling_block(nn.Module):
