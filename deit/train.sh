2e56437e52016b6c81ce39d073c65559d169e497
Lee Seung Hoon <aanna0701@gmail.com>
2021-06-07 08:56:37 +0000

    update

--- deit/train.sh
+++ deit/train.sh
@@ -2,7 +2,7 @@
 
 for s in 1 2 3
 do
-	python main.py --data_path /workspace/dataset/imnet --gpu 0 --lr 0.0005 --model DeiT --seed ${s} --depth 12 --channel 192 --heads 3 --data_set IMNET --tag G_DeiT_Ti --data_set IMNET
-	python main.py --data_path /workspace/dataset/imnet --gpu 0 --lr 0.0005 --model DeiT --seed ${s} --depth 12 --channel 384 --heads 6 --data_set IMNET --tag G_DeiT_B --data_set IMNET
+	#python main.py --data_path /workspace/dataset/imnet --gpu 0 --lr 0.0005 --model g-vit --seed ${s} --depth 12 --channel 192 --heads 3 --tag g-deit-Ti
+	python main.py --data_path /workspace/dataset/imnet --gpu 0 --lr 0.0005 --model g-vit --seed ${s} --depth 12 --channel 384 --heads 6 --tag g-deit-B 
 done
 
