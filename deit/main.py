53ce7d237d9556d17288421a218fc5e4cd3d3695
Lee Seung Hoon <aanna0701@gmali.com>
2021-06-07 18:37:33 +0900

    update

--- deit/main.py
+++ deit/main.py
@@ -159,7 +159,7 @@ def get_args_parser():
                         help='start epoch')
     parser.add_argument('--eval', action='store_true', help='Perform evaluation only')
     parser.add_argument('--dist-eval', action='store_true', default=False, help='Enabling distributed evaluation')
-    parser.add_argument('--num_workers', default=4, type=int)
+    parser.add_argument('--num_workers', default=10, type=int)
     parser.add_argument('--gpu', default=0, type=int)
     parser.add_argument('--pin-mem', action='store_true',
                         help='Pin CPU memory in DataLoader for more efficient (sometimes) transfer to GPU.')
