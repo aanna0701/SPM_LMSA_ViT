8a13ab7540d15f97e23d9ec90e5545c5abf2083e
Lee Seung Hoon <aanna0701@gmali.com>
2021-06-09 02:31:05 +0900

    update

--- models/vits.py
+++ models/vits.py
@@ -324,11 +324,6 @@ class GA_block(nn.Module):
         # self.mlp_nodes = nn.Linear(in_channels, in_channels, bias=False)
         self.linear = nn.Linear(in_channels, in_channels, bias=False)
         self._init_weights(self.linear)
-        self.mlp1 = nn.Linear(in_channels, in_channels//2, bias=False)
-        self._init_weights(self.mlp1)
-        self.mlp2 = nn.Linear(in_channels//2, in_channels, bias=False)
-        self._init_weights(self.mlp2)
-        self.relu = nn.ReLU()
         
     def _init_weights(self,layer):
         nn.init.kaiming_normal_(layer.weight)
@@ -358,19 +353,17 @@ class GA_block(nn.Module):
         node_global = self.avgpool(nodes.permute(0, 2, 1)).permute(0, 2, 1)     # (B, 1, C)
         
         cat_global = torch.cat([edge_global, node_global], dim=1)   # (B, 2, C)
-        cat_hidden = self.relu(self.mlp1(cat_global))
-        cat_out = self.mlp2(cat_hidden)
-        # norm = torch.norm(cat_global, dim=2, keepdim=True, p=1)   # (B, 2, 1)
-        # norm = norm.expand(norm.size(0), 2, nodes.size(2))  # (B, 2, C)
-        # scale_edge = torch.div(cat_global, norm) * self.in_dimension
+        norm = torch.norm(cat_global, dim=2, keepdim=True, p=1)   # (B, 2, 1)
+        norm = norm.expand(norm.size(0), 2, nodes.size(2))  # (B, 2, C)
+        scale_edge = torch.div(cat_global, norm) * self.in_dimension
         
         
-        # edge_global = scale_edge[:, (0,)] 
-        # node_global = scale_edge[:, (1,)]
+        edge_global = scale_edge[:, (0,)] 
+        node_global = scale_edge[:, (1,)]
         
         
-        # channel_attention = edge_global + node_global # (B, 1, C)
-        channel_attention = torch.sum(cat_out, dim=1, keepdim=True) # (B, 1, C)
+        channel_attention = edge_global + node_global # (B, 1, C)
+        # channel_attention = torch.sum(cat_out, dim=1, keepdim=True) # (B, 1, C)
         
         
         
@@ -472,7 +465,7 @@ class Transformer_Block(nn.Module):
             '''
             edge_per_node = x_MHSA
             
-            x_inter2 = self.GA(x_res1, cls_token, edge_per_node, dropout)
+            x_inter2 = self.GA(x_res1, x_inter1[:, (0,)], edge_per_node, dropout)
             x_inter2 = self.normalization_GA(x_inter2)
             
         
