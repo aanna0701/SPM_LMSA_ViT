fdfe1cc1af7a4288b357420aa3a89b58b7d87efb
Lee Seung Hoon <aanna0701@gmali.com>
2021-06-10 13:56:21 +0900

    update

---
+++ utils/logger.py
@@ -0,0 +1,11 @@
+from collections import OrderedDict
+
+class logger_dict():
+    def __init__(self):
+        self.dict = OrderedDict()
+    
+    def append(self, key, value):
+        self.dict[key] = value
+        
+    def print(self, logger):
+        logger.debug(self.dict)
