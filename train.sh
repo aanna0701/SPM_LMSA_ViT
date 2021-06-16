e5df1ba6b716d0bbf7d8943e8fffeb554a30208b
Lee Seung Hoon <aanna0701@gmali.com>
2021-06-16 19:44:48 +0900

    update

--- train.sh
+++ train.sh
@@ -1,8 +1,16 @@
 #!/bin/zsh
 
 
+# for s in 3 4 5
+# do
+# 	python main.py --depth 12 --channel 96 --heads 3 --gpu 0 --lr 0.0005 --model vit --tag vit-cifar10-12-96-aa-mu --seed ${s} --dataset CIFAR10 --ls --aa --mu
+# 	python main.py --depth 12 --channel 144 --heads 3 --gpu 1 --lr 0.0005 --model vit --tag vit-cifar10-12-144-aa-mu --seed ${s} --dataset CIFAR10 --ls --aa --mu
+# done
+
+
 for s in 3 4 5
 do
-	python main.py --depth 12 --channel 96 --heads 3 --gpu 0 --lr 0.0005 --model vit --tag vit-cifar10-12-96 --seed ${s} --dataset CIFAR10 --ls
-#	python main.py --depth 12 --channel 144 --heads 3 --gpu 1 --lr 0.0005 --model vit --tag vit-cifar10-12-144 --seed ${s} --dataset CIFAR10 --ls
+	python main.py --depth 12 --heads 3 --channel 96 --gpu 0 --model vit --tag cifar10-12-96-aa-mu --seed ${s} --dataset CIFAR10 --ls --aa --mu
+	# python main.py --depth 12 --heads 3 --channel 96 --gpu 1 --model g-vit --tag cifar10-12-96-aa-mu --seed ${s} --dataset CIFAR10 --ls --aa --mu
+	# python main.py  --depth 12 --heads 3 --channel 96 --channel 144 --gpu 1 --model g-vit --tag cifar10-12-144-aa-mu --seed ${s} --dataset CIFAR10 --ls --aa --mu 
 done
