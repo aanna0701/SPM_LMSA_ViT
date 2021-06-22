7c9accd8cf025317526eccfce73385820c68c612
Lee Seung Hoon <aanna0701@gmali.com>
2021-06-22 18:24:21 +0900

    update

--- train.sh
+++ train.sh
@@ -8,12 +8,12 @@
 # done
 
 
-for s in 3
+for s in 3 4 5
 do
-	python main.py --depth 12 --heads 3 --channel 72 --lr 0.003 --gpu 1 --model g-vit --tag 12-72-aa-mu-cm-sd-ra-re-global_attribute_max --seed ${s} --dataset CIFAR100 --ls --aa --mu --sd 0.1 --ra 3 --cm --re 0.5
-#	python main.py --depth 12 --heads 3 --channel 96 --lr 0.003 --gpu 0 --model g-vit --tag 12-96-aa-mu-cm-sd-ra-re-global_attribute_max --seed ${s} --dataset CIFAR100 --ls --aa --mu --sd 0.1 --ra 3 --cm --re 0.5
-#	python main.py --depth 12 --heads 3 --channel 144 --lr 0.003 --gpu 0 --model g-vit --tag 12-144-aa-mu-cm-sd-ra-re-global_attribute_max --seed ${s} --dataset CIFAR100 --ls --aa --mu --sd 0.1 --ra 3 --cm --re 0.5
-	python main.py --depth 12 --heads 3 --channel 192 --lr 0.003 --gpu 1 --model g-vit --tag 12-192-aa-mu-cm-sd-ra-re-global_attribute_max --seed ${s} --dataset CIFAR100 --ls --aa --mu --sd 0.1 --ra 3 --cm --re 0.5
+#	python main.py --depth 12 --heads 3 --channel 72 --lr 0.001 --gpu 1 --model g-vit --tag 12-72-aa-mu-cm-sd-ra-re-no_cls_meanpool --seed ${s} --dataset CIFAR100 --ls --aa --mu --sd 0.1 --ra 3 --cm --re 0.25
+	python main.py --depth 12 --heads 3 --channel 96 --lr 0.001 --gpu 0 --model g-vit --tag 12-96-aa-mu-cm-sd-ra-re-no_cls_meanpool --seed ${s} --dataset CIFAR100 --ls --aa --mu --sd 0.1 --ra 3 --cm --re 0.25
+	python main.py --depth 12 --heads 3 --channel 144 --lr 0.001 --gpu 0 --model g-vit --tag 12-144-aa-mu-cm-sd-ra-re-no_cls_meanpool --seed ${s} --dataset CIFAR100 --ls --aa --mu --sd 0.1 --ra 3 --cm --re 0.25
+#	python main.py --depth 12 --heads 3 --channel 192 --lr 0.001 --gpu 1 --model g-vit --tag 12-192-aa-mu-cm-sd-ra-re-no_cls_meanpool --seed ${s} --dataset CIFAR100 --ls --aa --mu --sd 0.1 --ra 3 --cm --re 0.25
 done
 
 for s in 3
