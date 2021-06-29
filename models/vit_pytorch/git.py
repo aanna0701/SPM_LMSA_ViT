b74ecfd40c0bcc66145e0b25a7b0004cdc568a0f
Lee Seung Hoon <aanna0701@gmali.com>
2021-06-29 14:27:40 +0900

    update

--- models/vit_pytorch/git.py
+++ models/vit_pytorch/git.py
@@ -106,7 +106,7 @@ class Transformer(nn.Module):
             self.hidden_states[str(i)] = x
         return x
 
-class ViT(nn.Module):
+class GiT(nn.Module):
     def __init__(self, *, img_size, patch_size, num_classes, dim, depth, heads, mlp_dim, pool = 'cls', channels = 3, dim_head = 64, dropout = 0., emb_dropout = 0., stochastic_depth=0.):
         super().__init__()
         image_height, image_width = pair(img_size)
