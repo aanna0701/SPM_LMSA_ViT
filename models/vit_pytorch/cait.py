4b40baf9d2e1a31eac8e1698b68d323fdd54abb9
Lee Seung Hoon <aanna0701@gmali.com>
2021-08-10 11:14:27 +0900

    update

--- models/vit_pytorch/cait.py
+++ models/vit_pytorch/cait.py
@@ -102,8 +102,8 @@ class Attention(nn.Module):
         dots = torch.mul(einsum('b h i d, b h j d -> b h i j', q, k), scale.unsqueeze(0).unsqueeze(-1).unsqueeze(-1).expand((x.size(0), self.heads, 1, 1)))
     
         dots = einsum('b h i j, h g -> b g i j', dots, self.mix_heads_pre_attn)    # talking heads, pre-softmax
-        print(self.mask.shape)
-        dots[:, :, self.mask, self.mask] = self.inf
+        
+        dots[:, :, self.mask[:, 0], self.mask[:, 1]] = self.inf
         
         attn = self.attend(dots)        
         attn = einsum('b h i j, h g -> b g i j', attn, self.mix_heads_post_attn)   # talking heads, post-softmax
