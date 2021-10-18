ed9997c11bc7ce928ba676a0330e184393de36c8
Lee Seung Hoon <aanna0701@gmali.com>
2021-10-18 16:16:05 +0900

    update

--- measure_cost.py
+++ measure_cost.py
@@ -75,16 +75,19 @@ def main():
     
     inputs = torch.randn(128, 3, img_size, img_size).cuda(GPU)
     repetitions=100
+    warmup = 200
+    
     total_time = 0
     with torch.no_grad():
-        for rep in range(repetitions):
-            starter, ender = torch.cuda.Event(enable_timing=True),   torch.cuda.Event(enable_timing=True)
-            starter.record()
-            _ = model(inputs)
-            ender.record()
-            torch.cuda.synchronize()
-            curr_time = starter.elapsed_time(ender)/1000
-            total_time += curr_time
+        for rep in range(repetitions+warmup):
+            if not rep < warmup:
+                starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
+                starter.record()
+                _ = model(inputs)
+                ender.record()
+                torch.cuda.synchronize()
+                curr_time = starter.elapsed_time(ender)/1000
+                total_time += curr_time
     Throughput =   (repetitions*128)/total_time
     print('Final Throughput:',Throughput)
         
--- utils/throughput.py
+++ utils/throughput.py
@@ -7,17 +7,20 @@ import torch
 def throughput(model, img_size, args):
         
     inputs = torch.randn(args.batch_size, 3, img_size, img_size).cuda(args.gpu)
-    repetitions=100
+    repetitions=1000
+    warmup = 200
+    
     total_time = 0
     with torch.no_grad():
-        for rep in range(repetitions):
-            starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
-            starter.record()
-            _ = model(inputs)
-            ender.record()
-            torch.cuda.synchronize()
-            curr_time = starter.elapsed_time(ender)/1000
-            total_time += curr_time
+        for rep in range(repetitions+warmup):
+            if not rep < warmup:
+                starter, ender = torch.cuda.Event(enable_timing=True), torch.cuda.Event(enable_timing=True)
+                starter.record()
+                _ = model(inputs)
+                ender.record()
+                torch.cuda.synchronize()
+                curr_time = starter.elapsed_time(ender)/1000
+                total_time += curr_time
     Throughput =   (repetitions*128)/total_time
     
     
