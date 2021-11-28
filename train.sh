64469c2307e3b46c109da827e500f2279b2d8156
Lee Seung Hoon <aanna0701@gmali.com>
2021-11-28 16:52:12 +0900

    update

--- train.sh
+++ train.sh
@@ -3,5 +3,5 @@ do
 
 #	python main.py --heads 12 --channel 192 --depth 9 --lr 0.001 --gpu 0 --model pit --tag Residual_SPT_LSA_learnable_swin --seed ${s} --dataset CIFAR100 --ls --aa --mu --sd 0.1 --ra 3 --cm --re 0.25 --data_path ../dataset 
 
-	python main.py --heads 12 --channel 192 --depth 9 --lr 0.001 --gpu 0 --type_trans affine --model swin --tag SPT_new_PC_sequential --init_noise_trans 0 --init_noise_scale 0 --scale 1 --is_trans_learn --seed ${s} --dataset CIFAR100 --ls --aa --mu --sd 0.1 --ra 3 --cm --re 0.25 --data_path ../dataset  --n_trans 4 --lam 1e-3
+	python main.py --heads 12 --channel 192 --depth 9 --lr 0.001 --gpu 0 --model swin --tag test --init_noise_trans 0 --init_noise_scale 0 --scale 0  --lam 0 --seed ${s} --dataset T-IMNET --ls --aa --mu --sd 0.1 --ra 3 --cm --re 0.25 --data_path ../dataset  --n_trans 4
 done
