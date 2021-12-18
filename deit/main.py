649b799f5938081b71669ea8d696e07636c4d72b
Lee Seung Hoon <aanna0701@gmali.com>
2021-12-18 20:45:09 +0900

    udpate

--- deit/main.py
+++ deit/main.py
@@ -223,7 +223,7 @@ def main(args):
     #     drop_block_rate=None,
     # )
     if args.model == 'vit':
-        from ..models.vit_pytorch.vit import ViT     
+        from models.vit_pytorch.vit import ViT     
         model = ViT(img_size=args.input_size, patch_size = 16, num_classes=args.nb_classes, dim=192, 
                     mlp_dim_ratio=2, depth=12, heads=3, dim_head=192//3, pe_dim=64,
                     dropout=args.drop, stochastic_depth=args.drop_path, is_base=args.is_base, eps=0, merging_size=4,
