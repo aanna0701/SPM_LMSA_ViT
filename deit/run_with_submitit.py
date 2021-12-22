dba3648b652be3d512e1ad905620d353c395ed2e
Lee Seung Hoon <aanna0701@gmali.com>
2021-12-22 21:03:47 +0900

    update

--- deit/run_with_submitit.py
+++ deit/run_with_submitit.py
@@ -28,9 +28,9 @@ def parse_args():
 
 
 def get_shared_folder() -> Path:
-    user = os.getenv("USER")
-    if Path("/checkpoint/").is_dir():
-        p = Path(f"/checkpoint/{user}/experiments")
+    
+    if Path(os.getcwd()+'/shared').is_dir():
+        p = Path(os.getcwd()+'/shared'+"/experiments")
         p.mkdir(exist_ok=True)
         return p
     raise RuntimeError("No shared folder available")
