2decb8dc820f0aab37617ba55058365941faee5d
Lee Seung Hoon <aanna0701@gmali.com>
2022-01-02 14:54:05 +0900

    update

--- train.sh
+++ train.sh
@@ -1,8 +1,7 @@
 for s in 5 6 7
 do 
 
-	python main.py --lr 0.001 --gpu 0 --model swin --scale 0  --gam 0 --lam 0 --seed ${s} --dataset CIFAR100 --ls --aa --mu --sd 0.1 --ra 3 --cm --re 0.25 --data_path ../dataset 
-	python main.py --heads 12 --channel 192 --depth 9 --lr 0.003 --gpu 0 --model vit --scale 0  --gam 0 --lam 0 --seed ${s} --dataset CIFAR100 --ls --aa --mu --sd 0.1 --ra 3 --cm --re 0.25 --data_path ../dataset
-
+#	python main.py --lr 0.001 --gpu 0 --model swin --scale 0 --lam 0 --seed ${s} --dataset CIFAR100 --ls --aa --mu --sd 0.1 --ra 3 --cm --re 0.25 --data_path ../dataset 
+	python main.py --heads 12 --channel 192 --depth 9 --lr 0.003 --gpu 0 --model vit --scale 0 --lam 0.1 --seed ${s} --dataset CIFAR100 --ls --aa --mu --sd 0.1 --ra 3 --cm --re 0.25 --data_path ../dataset
 
 done
